2061b31d6e96388a11f42daf42e86f55204d1d2a
Yifeng Chu <melancholymirth@gmail.com>
2016-11-06 10:19:15 -0600

    pixel value changed

--- mp3_image/bin/imrecog_func.py
+++ mp3_image/bin/imrecog_func.py
@@ -1,10 +1,50 @@
 import numpy as np
 import matplotlib.pyplot as pl
+import os
 WHITE = ' '
 GRAY = '+'
 BLACK = '#'
+FG = 1
+BG = 0
 IM_SIZE = 28
-def train(train_data):
+
+def get_data():
+    #set path
+    bin_path = os.getcwd()
+    mp_path = os.path.dirname(bin_path)
+    data_path = mp_path + '/database/' 
+    train_images_file = data_path + 'trainingimages' 
+    train_labels_file = data_path + 'traininglabels'
+    test_images_file = data_path + 'testimages'
+    test_labels_file = data_path + 'testlabels'
+    #get data
+    f = open(train_images_file, 'r')
+    train_images = np.array([list(eachline.strip('\n')) 
+                            for eachline in f])
+    f.close()
+    f = open(train_labels_file, 'r')
+    train_labels = np.genfromtxt(train_labels_file, dtype=int)
+    f.close()
+    f = open(test_images_file, 'r')
+    test_images = np.array([list(eachline.strip('\n')) 
+                            for eachline in f])
+    f.close()
+    f = open(test_labels_file, 'r')
+    test_labels = np.genfromtxt(test_labels_file, dtype=int)
+    f.close()
+    data = {'train': {'images': train_images, 'labels': train_labels},
+            'test': {'images': test_images, 'labels': test_labels}}
+    return data 
+
+def extract_feature(data):
+    feature = np.zeros(data['images'].shape)
+    fg_idx = np.logical_or(data['images']==GRAY, data['images']==BLACK)
+    bg_idx = data['images'] == WHITE
+    feature[fg_idx] = 1
+    feature[bg_idx] = 0
+    return feature
+
+def train(feature, train_data):
     """
     train to get the feature, the feature would be a dictionary,
     the key would be the three possible chars in the image.
@@ -12,22 +52,33 @@ def train(train_data):
     represents the class
     """
     #initialization
-    likelihood = {WHITE: None, GRAY: None, BLACK: None}
-    feature = {pixel_value: est_likelihood(pixel_value, train_data)
-               for pixel_value in [WHITE, GRAY, BLACK]} 
-    
-def est_likelihood(pixel_value, train_data):
+    model = {'likelihoods': None, 'priors': None}
+    model['likelihoods'] = {pixel_value: est_likelihood(pixel_value, feature,
+                            train_data) for pixel_value in [FG, BG]} 
+    num_train_samples = train_data['image'].shape[0] / IM_SIZE 
+    model['prior'] = np.array([(train_data['labels'] == class_).sum()
+            for class_ in range(10)]) / float(num_train_samples)
+
+    return model
+
+def est_likelihood(pixel_value, feature, train_data):
     """
     numerator represents the # of times pixel has specific value
     from training example from this class
     denominator represents the # of training example from this class
     """
+    likelihood = np.zeros((10, IM_SIZE, IM_SIZE))
     for class_ in range(10):
-        numerator = (train_data.reshape(-1, IM_SIZE, IM_SIZE)
-                     == pixel_value).sum(axis=0)
-        denominator = (train_data[label] == class_).sum() 
-        likelihood[pixel_value][:, :, class_] = numerator / denominator
+        class_idx = train_data['labels'] == class_
+        numerator = (feature.reshape(-1, IM_SIZE, IM_SIZE)[class_idx, :, :] 
+                     == pixel_value).sum(axis=0) 
+        denominator = class_idx.sum() 
+        likelihood[class_, :, :] = smooth(numerator, denominator)
     return likelihood
+
+def smooth(numerator, denominator):
+    k, V = 1, 2
+    return (numerator + k) / (float(denominator) + k*V)
 
 def test():
     pass
